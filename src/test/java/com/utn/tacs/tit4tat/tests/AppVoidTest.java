d0c2a8d879dd2e76d5394c789a8ae9459614575b
waltercrdz <waltercrdz@gmail.com>
2014-09-04 20:16:53 -0300

    Test para la creacion de src/test/java

---
+++ src/test/java/com/utn/tacs/tit4tat/tests/AppVoidTest.java
@@ -0,0 +1,19 @@
+package com.utn.tacs.tit4tat.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppVoidTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
